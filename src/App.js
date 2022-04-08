9f23fc379b3565660a20c50ad9651b42d281f800
“kiranrathor30697” <kiranrathor30697@gmail.com>
2022-04-08 12:16:24 +0530

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer, useState } from 'react';
+
+let ReducerFunction = (x,action)=>{
+  if(action.type==='Increment'){
+    return x = [x+1,"kkk"]
+  }else if(action.type==='decrement'){
+    return (x-1);
+  }else if(action.type==='changedata'){
+    return x = "Anjali"
+  }else{
+    return x;
+  }
+}
 
 function App() {
+  const [x,y] = useState(["200","kiran"])
+  
+  const [newdata,dispatch] = useReducer(ReducerFunction,x)
+
+  let clickHere = ()=>{
+    //alert('Click here');
+    dispatch({
+      type:"Increment"
+    })
+  }
+  let clickHere2 = ()=>{ 
+    dispatch({
+      type:"decrement"
+    })
+  }
+  let clickHere3 = ()=>{
+    dispatch({
+      type:"changedata"
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+
+      {newdata}<br />
+      <button className="btn btn-success" onClick={()=>{clickHere()}}>click Here</button><br />
+      <button className="btn btn-success" onClick={()=>{clickHere2()}}>click Here</button><br />
+      <button className="btn btn-success" onClick={()=>{clickHere3()}}>click Here</button>
     </div>
   );
 }
